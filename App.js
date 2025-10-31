565cad2c6fb27b77d4a7b6fe748e064253becf61
Alisa Ermel <alisa.ermel@gmail.com>
2025-10-31 15:41:44 +0200

    Add Google font

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useFonts, Audiowide_400Regular } from '@expo-google-fonts/audiowide';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Audiowide_400Regular,
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text style={[styles.text, { fontFamily: fontsLoaded ? "Audiowide_400Regular" : undefined, }]}>Welcome to Orbit!</Text>
+
+      <Image
+        source={require('./assets/images/planet00.png')}
+        style={styles.image}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 20,
+  },
+  image: {
+    height: '50%',
+    width: '100%',
+  }
 });
