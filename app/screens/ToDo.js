1651eb6d8c73ff42b78094ad480a9748acb9b615
Alisa Ermel <alisa.ermel@gmail.com>
2025-11-09 17:15:47 +0200

    Make a category Picker

--- app/screens/ToDo.js
+++ app/screens/ToDo.js
@@ -2,8 +2,19 @@ import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet, Image } from "react-native";
 import { TextInput } from "react-native-paper";
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useFonts, Audiowide_400Regular } from "@expo-google-fonts/audiowide";
 
 export default function ToDo() {
+    //Font
+    const [fontsLoaded] = useFonts({
+        Audiowide_400Regular,
+    });
+
+    //For Picker
+    const [open, setOpen] = useState(false);
+    const [category, setCategory] = useState(null);
+
     const [todo, setToDo] = useState({
         name: "",
         category: "",
@@ -14,7 +25,7 @@ export default function ToDo() {
     return (
         <SafeAreaView style={styles.container}>
 
-            {/* Image of thw section */}
+            {/* Image of the section */}
             <Image
                 source={require("../../assets/images/todo.png")}
                 style={styles.image}
@@ -33,16 +44,23 @@ export default function ToDo() {
             />
 
 
-            {/* TextInput for Category of the ToDo */}
-            <TextInput
-                placeholder="Category"
-                value={todo.category}
-                onChangeText={text => setToDo({ ...todo, category: text })}
-                mode="outlined"
-                outlineColor="#41111d71"
-                activeOutlineColor="#41111d"
-                outlineStyle={{ borderWidth: 5 }} // <-- makes the border thicker
-                style={styles.textInput}
+            {/* DropDown Picker for Category of the ToDo */}
+            <DropDownPicker
+                open={open}
+                value={category}
+                items={categories.map(cat => ({ label: cat, value: cat }))}
+                setOpen={setOpen}
+                setValue={setCategory}
+                placeholder="Select a category"
+                style={[
+                    styles.dropdown,
+                    open ? styles.dropdownOpen : null  // change border/background when open
+                ]}
+                dropDownContainerStyle={[
+                    styles.dropdownContainer,
+                    open ? styles.dropdownContainerOpen : null // change dropdown container when open
+                ]}
+                textStyle={{ fontFamily: "Audiowide_400Regular" }}
             />
 
 
@@ -65,5 +83,27 @@ const styles = StyleSheet.create({
     textInput: {
         width: "80%",
         marginVertical: 5,
-    }
+    },
+    dropdown: {
+        width: "80%",
+        marginVertical: 5,
+        borderWidth: 5,
+        borderColor: "#41111d71",
+        borderRadius: 4,
+        height: 56,  // match TextInput
+        alignSelf: "center",
+    },
+    dropdownContainer: {
+        width: "80%",
+        borderWidth: 5,
+        borderColor: "#41111d71",
+        borderRadius: 4,
+        alignSelf: "center",
+    },
+    dropdownOpen: {
+        borderColor: "#41111d",   // e.g., tomato border when pressed
+    },
+    dropdownContainerOpen: {
+        borderColor: "#41111d",    // match border color of picker
+    },
 })
